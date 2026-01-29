780640d678203aa019d03d2dead838b45e698fc7
PogchamP <Kelvin@Chongs-MacBook-Air.local>
2026-01-29 23:37:18 +0800

    Updated entity display details logic and course modal input field(Programmes)

--- sql/course.sql
+++ sql/course.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS courses (
     course_code VARCHAR(20) PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
     lecturer_id VARCHAR(20) NOT NULL,
+    programme_name VARCHAR(100) NOT NULL,
     course_year INT NOT NULL,
-    course_semester INT NOT NULL,
     FOREIGN KEY (lecturer_id) REFERENCES lecturers(lecturer_id)
 );
